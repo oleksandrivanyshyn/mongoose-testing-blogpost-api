e5ca36bb4e89f3eeb1e5d20e74980e7c5759d391
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-02 22:22:41 +0300

    test: update user creation test to handle asynchronous save and add database setup

--- test/create_test.js
+++ test/create_test.js
@@ -4,6 +4,9 @@ const User = require('../src/user');
 describe('Creating records', () => {
   it('saves a user', () => {
     const joe = new User({ name: 'Joe' });
-    joe.save();
+    joe.save().then(() => {
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
