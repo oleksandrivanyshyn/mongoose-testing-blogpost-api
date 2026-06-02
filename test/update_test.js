f77c48c1dd369c344a9966d1cdcc7ae4d88b3a2f
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-02 23:37:11 +0300

    test: add tests for various Mongoose update methods

--- test/update_test.js
+++ test/update_test.js
@@ -24,4 +24,13 @@ describe('Updating records', () => {
   it('A model instance can update', (done) => {
     assertName(joe.updateOne({ name: 'Alex' }), done);
   });
+  it('A model class can update', (done) => {
+    assertName(User.updateMany({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+  it('A model class can update one record', (done) => {
+    assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
+  });
+  it('A model class can find a record with an Id and update', (done) => {
+    assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
 });
