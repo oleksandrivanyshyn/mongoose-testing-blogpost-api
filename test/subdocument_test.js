728daf927c937214c7f3cf39cb52be884dc8f8ba
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-03 00:18:07 +0300

    test: add test for removing subdocuments

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -26,4 +26,24 @@ describe('Subdocuments', () => {
         done();
       });
   });
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }],
+    });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        user.posts.pull(post);
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
 });
