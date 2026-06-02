981b99a691bf05a552631997003fe2d6676065dd
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-02 22:32:00 +0300

    chore: add Prettier as a dev dependency and create `.prettierrc` config, apply formatting across codebase

--- src/user.js
+++ src/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name: String
+  name: String,
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/test_helper.js
+++ test/test_helper.js
@@ -3,16 +3,18 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 before((done) => {
-    mongoose.connect('mongodb://localhost/users_test');
-    mongoose.connection
-        .once('open', () => {done()})
-        .on('error', (error) => {
-            console.warn('Warning', error);
-        });
-})
-
-
+  mongoose.connect('mongodb://localhost/users_test');
+  mongoose.connection
+    .once('open', () => {
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
 
 beforeEach((done) => {
-    mongoose.connection.collections.users.drop().then(() => {done()});
-})
+  mongoose.connection.collections.users.drop().then(() => {
+    done();
+  });
+});
