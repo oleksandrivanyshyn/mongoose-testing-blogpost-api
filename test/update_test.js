5c3d22535572b943216b689acfe83d9dfb5d9c7a
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-02 23:51:30 +0300

    test: add test for incrementing user post count using Mongoose update methods

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe', likes: 0 });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
   function assertName(operation, done) {
@@ -32,5 +32,14 @@ describe('Updating records', () => {
   });
   it('A model class can find a record with an Id and update', (done) => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their post count incremented by 1', (done) => {
+    User.updateMany({ name: 'Joe' }, { $inc: { postCount: 1 } }).then(() =>
+      User.findOne({ name: 'Joe' }).then((user) => {
+        assert(user.postCount === 1);
+        done();
+      }),
+    );
   });
 });
