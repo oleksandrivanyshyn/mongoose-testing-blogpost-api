684521bc6724ad2e456c9f9838988a1349cbee73
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-03 11:34:40 +0300

    feat: add schemas for comments and blog posts

---
+++ src/blogPost.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+  title: String,
+  content: String,
+  comments: [{ type: Schema.Types.ObjectId, ref: 'comment' }],
+});
+
+const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+
+module.exports = BlogPost;
