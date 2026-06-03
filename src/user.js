a5324fb740b3d88634f17e1e2b02949fff3a5570
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-03 13:24:03 +0300

    feat: cascade delete blog posts when user is removed

--- src/user.js
+++ src/user.js
@@ -18,6 +18,12 @@ const UserSchema = new Schema({
 UserSchema.virtual('postCount').get(function () {
   return this.posts.length;
 });
+
+UserSchema.pre('remove', function (next) {
+  const BlogPost = mongoose.model('blogPost');
+  BlogPost.deleteMany({ _id: { $in: this.blogPosts } }).then(() => next());
+});
+
 const User = mongoose.model('user', UserSchema);
 
 module.exports = User;
